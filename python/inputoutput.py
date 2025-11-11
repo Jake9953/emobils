e8182df45282e999080025c61f2b50f0204595da
Jake9953 <jacobwambua793@gmail.com>
2025-11-11 15:17:08 +0300

    Update subproject commit to indicate a dirty state

---
+++ python/inputoutput.py
@@ -0,0 +1,14 @@
+name = input("Enter your name  ")
+
+print(f"Hello, welcome {name}")
+
+nums1 = input("Enter namba ya kwanza ")
+nums2 = input("Enter number ya pili ")
+
+print(f"The sum is: {int(nums1) + int(nums2)}")
+print(f"The difference is: {int(nums1) - int(nums2)}")
+print(f"The product is: {int(nums1) * int(nums2)}")
+print(f"The quotient is: {int(nums1) / int(nums2)}")  
+print(f"The modulus is: {int(nums1) % int(nums2)}")
+print(f"The exponent is: {int(nums1) ** int(nums2)}")   
+print(f"The floor division is: {int(nums1) // int(nums2)}")
